edf57fbe9c66503ba9e0e40b1219b0d0c4abc219
Erik Petersen <Huey@Erik-Petersens-MacBook-Pro.local>
2011-09-23 23:37:53 -0400

    updates from meeting, added photos

--- bryant.rb
+++ bryant.rb
@@ -16,3 +16,19 @@ end
 get '/why' do
 	erb :why
 end
+
+get '/continue' do
+	erb :continue
+end
+
+get '/face' do
+	erb :face
+end
+
+get '/donate' do
+  erb :donate
+end
+
+get '/topten' do
+  erb :topten
+end
